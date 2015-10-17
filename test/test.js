7beb33e918d3623fde0fe24f76f84c85ef98250d
Sylvain Baronnet <sylvain@studio.gd>
2015-10-18 00:26:16 +0200

    add test

--- test/test.js
+++ test/test.js
@@ -15,10 +15,29 @@ var test = function (input, output, opts, done) {
 
 describe('postcss-position-alt', function () {
 
-    /* Write tests here
+    it('test position absolute', function (done) {
+      test('a{ absolute: top 10px left 20px right 30px bottom 40px; }',
+           'a{ position: absolute; top: 10px; left: 20px; right: 30px; bottom: 40px; }', { }, done);
+    });
+    it('test position absolute no value', function (done) {
+      test('a{ absolute: top left; }',
+           'a{ position: absolute; top: 0; left: 0; }', { }, done);
+    });
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
+    it('test position fixed', function (done) {
+      test('a{ fixed: bottom 10% right 4em; }',
+           'a{ position: fixed; bottom: 10%; right: 4em; }', { }, done);
+    });
+
+
+    it('test auto and inherit value', function (done) {
+      test('a{ fixed: bottom auto right inherit left; }',
+           'a{ position: fixed; bottom: auto; right: inherit; left: 0; }', { }, done);
+    });
+
+    it('test relative', function (done) {
+      test('a{ relative: top -1px; }',
+           'a{ position: relative; top: -1px; }', { }, done);
+    });
 
 });
